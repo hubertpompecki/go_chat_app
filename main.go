dd7c9195a3ea8e43cf8cc326f8fa27d67a51fcf7
Hubert Pompecki <hpompecki@gmail.com>
2017-01-07 19:53:35 +0000

    Chapter 1 completed

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "text/template"
+    "path/filepath"
+    "sync"
+    "flag"
+    "github.com/hpompecki/trace"
+    "os"
+)
+
+// templ represents a single template
+type templateHandler struct {
+    once sync.Once
+    filename string
+    templ *template.Template
+}
+
+// ServeHTTP handles the HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    t.once.Do(func () {
+        t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+    })
+    t.templ.Execute(w, r)
+}
+
+func main() {
+    var addr = flag.String("addr", ":8080", "The addr of the application.")
+    flag.Parse()
+    r := newRoom()
+    r.tracer = trace.New(os.Stdout)
+    // root
+    http.Handle("/", &templateHandler{filename: "chat.html"})
+    http.Handle("/room", r)
+    // start the chatroom
+    go r.run()
+    // start the web server
+    log.Println("Starting web server on", *addr)
+    if err := http.ListenAndServe(*addr, nil); err != nil {
+        log.Fatal("ListenAndServe:", err)
+    }
+}
